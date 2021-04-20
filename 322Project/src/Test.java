adc8163ac685455f360fe507ed294fa9d0368a54
cwong2 <charles.wong@wsu.edu>
2021-04-20 14:27:39 -0700

    files

---
+++ 322Project/src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author wongh
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Testing");
+		System.out.println("Hello Testing");
+		System.out.println("Hello Testing");
+		
+	}
+}
